9888020e45ab8111944e398bbc0d44fb2b27d2be
tjlcast <phx108@sina.com>
2024-05-08 17:05:18 +0800

    feat: remove runtime.

--- docker/run.sh
+++ docker/run.sh
@@ -9,5 +9,13 @@ docker run -itd \
   --name code-pilot \
   -v $PROJECT_PATH/.env:/app/Code-Pilot/.env \
   -v $PROJECT_PATH/mydatabase.db:/app/Code-Pilot/mydatabase.db \
-  -p 9966:9966 \
+  -p 9866:9966 \
   code-pilot:0.0.1
+
+# run local dev.
+# docker run -itd \
+#   --name code-pilot \
+#   -v $PROJECT_PATH:/app/Code-Pilot \
+#   -p 9866:9966 \
+#   code-pilot:0.0.1
+
